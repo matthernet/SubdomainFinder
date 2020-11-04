e973dc5cb661608cac24cddad5684a1bf87c3188
matt <46228173+matthernet@users.noreply.github.com>
2020-11-04 20:10:09 +0100

    added improvements from version v0.2.0

--- certificatefinder.sh
+++ certificatefinder.sh
@@ -1,6 +1,11 @@
 #/bin/bash
 
-wget https://crt.sh/?Identity=%25.$1 -O data
+# SubdomainFinder v0.2.0
+
+echo "Searching subdomains for $1"
+
+{
+wget https://crt.sh/?Identity=%25.$1 -O data >&-
 
 cat data | grep -i '<TD><A' -B 1 | cut -d '<' -f 2 | grep $1 | cut -d '>' -f 2 >> cachelista
 
@@ -8,8 +13,15 @@ wget https://dns.bufferover.run/dns?q=.$1 -O data
 
 cat data | grep $1 | cut -d '"' -f 2 | cut -d "," -f 2 | grep '[[:alpha:]]' >> cachelista
 
+wget https://subdomainfinder.c99.nl/scans/`date +%Y-%m-%d`/$1 -O data
+
+cat data | grep -oP '(?<=<a).*?(?=</a>)' | grep noreferrer | cut -d ">" -f 2 >> cachelista
+
 rm data
 
+echo "Subdomains found for $1" > $1-subdoms
+echo "" > $1-subdoms
 cat cachelista | sort | uniq > $1-subdoms
 
 rm cachelista
+} &> /dev/null
