d2d8fb381722ef4429b39be39c174e253996d8f0
Matt <46228173+matthernet@users.noreply.github.com>
2022-08-08 09:22:11 +0300

    Update subdomainfinder.sh

--- subdomainfinder.sh
+++ subdomainfinder.sh
@@ -1,34 +1,37 @@
 #/bin/bash
 
-# SubdomainFinder v0.2.0
+if [ -z $1 ]; then
+	echo "Please specify a domain" && exit
+else
+    echo "Searching subdomains for $1"
 
-echo "Searching subdomains for $1"
+    {
+    wget https://crt.sh/?Identity=%25.$1 -O data
 
-{
-wget https://crt.sh/?Identity=%25.$1 -O data
-
-cat data | grep -i '<TD><A' -B 1 | cut -d '<' -f 2 | grep $1 | cut -d '>' -f 2 >> cachelista
-
-
-wget https://api.hackertarget.com/hostsearch/?q=$1 -O data
-
-cat data | cut -d "," -f 1 >> cachelista
+    cat data | grep -i '<TD><A' -B 1 | cut -d '<' -f 2 | grep $1 | cut -d '>' -f 2 >> cachelista
 
 
-wget https://dns.bufferover.run/dns?q=.$1 -O data
+    wget https://api.hackertarget.com/hostsearch/?q=$1 -O data
 
-cat data | grep $1 | cut -d '"' -f 2 | cut -d "," -f 2 | grep '[[:alpha:]]' >> cachelista
+    cat data | cut -d "," -f 1 >> cachelista
 
 
-wget https://subdomainfinder.c99.nl/scans/`date +%Y-%m-%d`/$1 -O data
+    wget -d --header="x-api-key: <your api key here>" https://tls.bufferover.run/dns?q=.$1 -O data
 
-cat data | grep -oP '(?<=<a).*?(?=</a>)' | grep noreferrer | cut -d ">" -f 2 >> cachelista
+    cat data | grep $1 | cut -d '"' -f 2 | cut -d "," -f 4 | grep '[[:alpha:]]' >> cachelista
 
-rm data
 
-echo "Subdomains found for $1" > $1-subdoms
-echo "" > $1-subdoms
-cat cachelista | sort | uniq > $1-subdoms
+    wget https://subdomainfinder.c99.nl/scans/`date +%Y-%m-%d`/$1 -O data
 
-rm cachelista
-} &> /dev/null
+    cat data | grep -oP '(?<=<a).*?(?=</a>)' | grep noreferrer | cut -d ">" -f 2 >> cachelista
+
+    rm data
+
+    echo "Subdomains found for $1" > $1-subdoms
+    echo "" > $1-subdoms
+    cat cachelista | sort | uniq > $1-subdoms
+
+    rm cachelista
+    } &> /dev/null
+    echo "Subdomains found for $1 in $PWD/$1-subdoms"
+fi
